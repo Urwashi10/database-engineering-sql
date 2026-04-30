9fc38edb78f5d094960d966ac28d7298ebe139d5
Urwashi10 <142391363+Urwashi10@users.noreply.github.com>
2026-04-30 21:46:49 +0530

    Create 02_sample_data.sql

---
+++ case_study_payroll/02_sample_data.sql
@@ -0,0 +1,12 @@
+-- Departments
+INSERT INTO departments VALUES (1, 'HR'), (2, 'IT');
+
+-- Employees
+INSERT INTO employees VALUES 
+(1, 'John', 1),
+(2, 'Alice', 2);
+
+-- Salaries
+INSERT INTO salaries VALUES 
+(1, 50000, 5000),
+(2, 60000, 6000);
